8cf2b43985ac9fa47cff4bd0fb8b3aeccc44595c
Jin Park <jpar303@gmail.com>
2023-07-12 23:38:51 +0900

    initialized: zustand, styled-component

---
+++ src/components/zustand/idStore.ts
@@ -0,0 +1,20 @@
+import {create } from "zustand";
+
+interface ID {
+  id: string;
+  setId: (id: string) => void;
+}
+
+const IdStore = create<ID>((set) => ({
+  id: "init",
+  setId: (id) => {
+    set((state) => ({ id: id }));
+  },
+  /*
+    setId: (by) => {
+    set((state) => ({ id: by }));
+  }, // 이게 더 보기 편할 수 있음
+  */
+}));
+
+export default IdStore;
