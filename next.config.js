03cf95efe2fa1934473179c6546ec0aceb4d281e
Jin <jpar303@gmail.com>
2023-10-24 00:58:52 +0900

    added: global-style, theme for styled-component

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {               // 추가
+  compiler: {
+    // 추가
     styledComponents: true, // 추가
   },
   images: {
     domains: ['www.nespresso.com', 'localhost'],
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,22 @@
 'use client'
 
-import React, { Fragment, memo, useState } from 'react'
-import styled from 'styled-components'
-
-import './globals.css'
-
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import { theme } from 'components/styles/theme'
+import GlobalStyle from 'components/styles/GlobalStyles'
 import Section1 from 'pages/section1'
 import Section2 from 'pages/section2'
 import Section3 from 'pages/section3'
 import Footer from 'components/footer/footer'
 
 export default function Home() {
-  const [no, setNo] = useState(0)
-
-  const WholeSection = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 100%;
-  `
-
   return (
-    <WholeSection>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Section1 />
       <Section2 />
       <Section3 />
       <Footer />
-    </WholeSection>
+    </ThemeProvider>
   )
 }
