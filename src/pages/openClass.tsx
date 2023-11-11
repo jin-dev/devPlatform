b90fab5d6271260484e3534bacf62584f13fe457
Jin <jpar303@gmail.com>
2023-11-11 23:57:44 +0900

    added: sample UI with tailwind css

--- src/pages/openClass.tsx
+++ src/pages/openClass.tsx
@@ -4,96 +4,103 @@ import '../app/globals.css'
 
 const about = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none" />
+    <section className="bg-dark-blue">
+      <div className="font-lato bg-white">
+        <div className="max-w-7xl mt-6 mb-6 mx-auto px-4 py-4 sm:px-6 lg:px-8 xl:px-0">
+          <div className="max-w-7xl mt-6 mb-6 mx-auto px-4 py-4 sm:px-6 lg:px-8 xl:px-0">
+            <div className="flex flex-col items-center md:items-start justify-start">
+              <h1 className="text-4xl font-bold pt-6 pb-3 text-left mb-5">
+                Upcoming events
+              </h1>
+            </div>
+            <div>
+              <div className="flex flex-col items-center md:items-start justify-start">
+                <div className="inline-flex items-center justify-cente bg-sky-800 mb-8 px-10 py-3 text-white">
+                  January. 2024
+                </div>
+              </div>
+              <div className="flex flex-wrap">
+                <div className="flex flex-wrap justify-between align-baseline bg-white overflow-hidden rounded-none w-full mb-10 cursor-pointer">
+                  <div className="flex !basis-1/6 md:!basis-1/12 flex-col justify-center items-center basis-1/12 border-solid border border-gray font-light relative">
+                    <p className="absolute top-0 left-0 w-full px-1 md:px-3 py-3 text-xs md:text-base text-center">
+                      Event
+                    </p>
+                    <p className="text-center text-gray">
+                      Mon
+                      <span className="block font-normal text-2xl md:text-4xl">
+                        01
+                      </span>
+                    </p>
+                  </div>
+                  <div className="flex flex-wrap basis-5/6 lg:basis-11/12">
+                    <div className="px-5 pt-5 pb-2 md:px-6 md:pt-6 md:basis-1/2 flex flex-col justify-between !p-5 md:!pd-10 !basis-full lg:!basis-1/2 bg-gray-100 order-2 lg:order-1">
+                      <p>2024-01-01</p>
+                      <h2 className="text-3xl font-semibold leading-10 font-normal">
+                        Nespresso Open Class
+                      </h2>
+                      <p className="text-base font-light my-4 line-clamp-4 !my-0">
+                        It is a well known free coffee by Nespresso Korea
+                      </p>
+                      <div className="mt-10 flex flex-wrap items-center justify-center justify-self-end justify-between">
+                        <p>Seoul, Korea</p>
+                        <p>FREE</p>
+                      </div>
+                    </div>
+                    <div className="basis-full lg:basis-1/2 !order-1 lg:!order-2">
+                      <div className="md:basis-1/2">
+                        <img src="https://www.nespresso.com/ecom/medias/sys_master/public/29408650821662/2311-festive-card-banner-ma-1120x630px.png?impolicy=small&imwidth=560&imdensity=1" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="flex flex-col items-center md:items-start justify-start">
+                <div className="inline-flex items-center justify-cente bg-sky-800 mb-8 px-10 py-3 text-white">
+                  February. 2024
+                </div>
+              </div>
+              <div className="flex flex-wrap">
+                <div className="flex flex-wrap justify-between align-baseline bg-white overflow-hidden rounded-none w-full mb-10 cursor-pointer">
+                  <div className="flex !basis-1/6 md:!basis-1/12 flex-col justify-center items-center basis-1/12 border-solid border border-gray font-light relative">
+                    <p className="absolute top-0 left-0 w-full px-1 md:px-3 py-3 text-xs md:text-base text-center">
+                      Event
+                    </p>
+                    <p className="text-center text-gray">
+                      Thurs
+                      <span className="block font-normal text-2xl md:text-4xl">
+                        07
+                      </span>
+                    </p>
+                  </div>
+                  <div className="flex flex-wrap basis-5/6 lg:basis-11/12">
+                    <div className="px-5 pt-5 pb-2 md:px-6 md:pt-6 md:basis-1/2 flex flex-col justify-between !p-5 md:!pd-10 !basis-full lg:!basis-1/2 bg-gray-100 order-2 lg:order-1">
+                      <p>2024-02-07</p>
+                      <h2 className="text-3xl font-semibold leading-10 font-normal">
+                        Nespresso Open Class - StarField -
+                      </h2>
+                      <p className="text-base font-light my-4 line-clamp-4 !my-0">
+                        It is a well known free coffee by Nespresso Korea
+                      </p>
+                      <div className="mt-10 flex flex-wrap items-center justify-center justify-self-end justify-between">
+                        <p>Hanam, Korea</p>
+                        <p>FREE</p>
+                      </div>
+                    </div>
+                    <div className="basis-full lg:basis-1/2 !order-1 lg:!order-2">
+                      <div className="md:basis-1/2">
+                        <img src="https://www.nespresso.com/shared_res/mos/free_html/kr/mainbanner/202311-referral-card-banner-1120x630px.png" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </section>
   )
 }
 
